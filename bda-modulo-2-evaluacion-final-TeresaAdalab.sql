1f46bf4895964525892d644b34d44207aa13bab9
TeresaAdalab <159554655+TeresaAdalab@users.noreply.github.com>
2024-05-18 09:10:30 +0200

    Ejercicio 1
    Resolución del ejercicio 1: Seleccionar los nombre de peliculas que sea diferente

--- bda-modulo-2-evaluacion-final-TeresaAdalab.sql
+++ bda-modulo-2-evaluacion-final-TeresaAdalab.sql
@@ -1,2 +1,10 @@
-
-/* Ejercicio 1: Selecciona todos los nombres de las películas sin que aparezcan duplicados.*/
+-- Introducción y uso de la base de datos
+USE sakila;
+/*
+Muestra las tablas que tiene la base de datos de sakila.
+*/
+SHOW TABLES;
+-- Ejercicio 1: Nombres de las películas sin que aparezcan duplicados.
+SELECT DISTINCT title as "nombre_peli" 
+FROM film;
+-- Ejercicio 1: Nombres de las películas sin que aparezcan duplicados.
